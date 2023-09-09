bef5baf83a76c890968c9532a1f801b15de5b2e5
Parthavi29 <72445987+Parthavi29@users.noreply.github.com>
2023-09-09 19:51:45 +0530

    Update module.py

--- module.py
+++ module.py
@@ -98,278 +98,283 @@ def text():
     cv.waitKey(0)
 
 #Essential Functions
-def grayscale():
+def gray():
     import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    cv.imshow('Park', img)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    cv.imshow('Gray', gray)
+    img=cv.imread('Photos/cat.jpg')
+    cv.imshow('Color',img)
+    gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    cv.imshow('Gray',gray)
     cv.waitKey(0)
-
+    
 def blur():
     import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    cv.imshow('Park', img)
-    blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
-    cv.imshow('Blur', blur)
+    img=cv.imread('Photos/park.jpg')
+    cv.imshow('Cat',img)
+    blur=cv.GaussianBlur(img,(3,3),cv.BORDER_DEFAULT)
+    cv.imshow('Blur',blur)
     cv.waitKey(0)
-
+    
 def edge_cascade():
     import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    cv.imshow('Park', img)
-    canny = cv.Canny(blur, 125, 175)
-    cv.imshow('Canny Edges', canny)
+    img=cv.imread('Photos/park.jpg')
+    cv.imshow('Cat',img)
+    canny=cv.Canny(img,125,175)
+    cv.imshow('Canny',canny)
     cv.waitKey(0)
 
 def dilate():
     import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    cv.imshow('Park', img)
-    dilated = cv.dilate(canny, (7,7), iterations=3)
-    cv.imshow('Dilated', dilated)
+    img=cv.imread('Photos/park.jpg')
+    cv.imshow('Cat',img)
+    canny=cv.Canny(img,125,175)
+    dilated=cv.dilate(img,(3,3),iterations=1)
+    #eroded=cv.erode(dilated,(3,3),iterations=1)
+    cv.imshow('Dilated',dilated)
+    #cv.imshow('Eroded',eroded)
     cv.waitKey(0)
-
+    
 def erode():
     import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    cv.imshow('Park', img)
-    eroded = cv.erode(dilated, (7,7), iterations=3)
-    cv.imshow('Eroded', eroded)
+    img=cv.imread('Photos/park.jpg')
+    cv.imshow('Cat',img)
+    canny=cv.Canny(img,125,175)
+    dilated=cv.dilate(img,(3,3),iterations=1)
+    eroded=cv.erode(dilated,(3,3),iterations=1)
+    #cv.imshow('Dilated',dilated)
+    cv.imshow('Eroded',eroded)
     cv.waitKey(0)
-
-def resize():
-    import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    cv.imshow('Park', img)
-    resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
-    cv.imshow('Resized', resized)
-    cv.waitKey(0)
-
-def crop():
-    import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    cv.imshow('Park', img)
-    cropped = img[50:200, 200:400]
-    cv.imshow('Cropped', cropped)
-    cv.waitKey(0)
-
-#Transformations
+    
+#Transformations  
 def translate():
-    import numpy as np
     import cv2 as cv
-    img = cv.imread('Photos/cat.jpg', 0)
-    rows, cols = img.shape[0],img.shape[1]
-    M = np.float32([[1, 0, 100], [0, 1, 50]])
-    dst = cv.warpAffine(img, M, (cols, rows))
-    cv.imshow('img', dst)
+    import numpy as np
+    img=cv.imread('Photos/park.jpg',0)
+    rows,cols=img.shape
+    M=np.float32([[1,0,100],[0,1,50]])
+    dst=cv.warpAffine(img,M,(cols,rows))
+    cv.imshow('Image',dst)
     cv.waitKey(0)
-    cv.destroyAllWindows()
-
-def reflection():
+    cv.destroyAllWindows
+    
+def reflection_xaxis():
     import numpy as np
     import cv2 as cv
     img = cv.imread('Photos/cat.jpg', 0)
-    rows, cols = img.shape[0],img.shape[1]
-    M = np.float32([[1,  0, 0],[0, -1, rows],[0,  0, 1]])
+    rows, cols = img.shape
+    #M = np.float32([[1,  0, 0],[0, -1, rows],[0,  0, 1]]) #vertical
+    M = np.float32([[-1, 0, cols], [0, 1, 0], [0, 0, 1]]) #horizontal
     reflected_img = cv.warpPerspective(img, M,(int(cols),int(rows)))
     cv.imshow('img', reflected_img)
     cv.imwrite('reflection_out.jpg', reflected_img)
     cv.waitKey(0)
-    cv.destroyAllWindows()
-
-def rotation():
+    cv.destroyAllWindows()  
+    
+def reflection_yaxis():
     import numpy as np
     import cv2 as cv
     img = cv.imread('Photos/cat.jpg', 0)
-    rows, cols = img.shape[0],img.shape[1]
+    rows, cols = img.shape
+    M = np.float32([[1,  0, 0],[0, -1, rows],[0,  0, 1]]) #vertical
+    #M = np.float32([[-1, 0, cols], [0, 1, 0], [0, 0, 1]]) #horizontal
+    reflected_img = cv.warpPerspective(img, M,(int(cols),int(rows)))
+    cv.imshow('img', reflected_img)
+    cv.imwrite('reflection_out.jpg', reflected_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows() 
+
+def rotate():
+    import numpy as np
+    import cv2 as cv
+    img = cv.imread('Photos/cat.jpg', 0)
+    rows, cols = img.shape
     M = np.float32([[1,  0, 0], [0, -1, rows], [0,  0, 1]])
     img_rotation = cv.warpAffine(img,cv.getRotationMatrix2D((cols/2, rows/2),30, 0.6),(cols, rows))
     cv.imshow('img', img_rotation)
     cv.imwrite('rotation_out.jpg', img_rotation)
     cv.waitKey(0)
     cv.destroyAllWindows()
-
-def scaling():
+    
+def shrink():
     import numpy as np
     import cv2 as cv
     img = cv.imread('Photos/cat.jpg', 0)
-    rows, cols = img.shape[0],img.shape[1]
-    img_shrinked = cv.resize(img, (250, 200),interpolation=cv.INTER_AREA)
+    rows, cols = img.shape
+    img_shrinked=cv.resize(img, (250, 200),interpolation=cv.INTER_AREA)
     cv.imshow('img', img_shrinked)
     img_enlarged = cv.resize(img_shrinked, None,fx=1.5, fy=1.5,interpolation=cv.INTER_CUBIC)
     cv.imshow('img', img_enlarged)
     cv.waitKey(0)
     cv.destroyAllWindows()
-
-def shearing_x_axis():
+    
+def enlarge():
     import numpy as np
     import cv2 as cv
     img = cv.imread('Photos/cat.jpg', 0)
-    rows, cols = img.shape[0],img.shape[1]
+    rows, cols = img.shape
+    img_enlarged = cv.resize(img_enlarged, None,fx=1.5, fy=1.5,interpolation=cv.INTER_CUBIC)
+    cv.imshow('img', img_enlarged)
+    img_enlarged = cv.resize(img_enlarged, None,fx=1.5, fy=1.5,interpolation=cv.INTER_CUBIC)
+    cv.imshow('img', img_enlarged)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def crop():
+    import cv2 as cv
+    img=cv.imread('Photos/park.jpg')
+    cv.imshow('Cat',img)
+    cropped=img[50:200,200:400]
+    cv.imshow('Cropped',cropped)
+    cv.waitKey(0)
+
+def shearing_x():
+    import numpy as np
+    import cv2 as cv
+    img = cv.imread('Photos/cat.jpg', 0)
+    rows, cols = img.shape
     M = np.float32([[1, 0.5, 0], [0, 1, 0], [0, 0, 1]])
     sheared_img = cv.warpPerspective(img, M, (int(cols*1.5), int(rows*1.5)))
     cv.imshow('img', sheared_img)
     cv.waitKey(0)
     cv.destroyAllWindows()
-
-def shearing_y_axis():
+    
+def shearing_y():
     import numpy as np
     import cv2 as cv
     img = cv.imread('Photos/cat.jpg', 0)
-    rows, cols = img.shape[0],img.shape[1]
+    rows, cols = img.shape
     M = np.float32([[1,   0, 0], [0.5, 1, 0], [0,   0, 1]])
     sheared_img = cv.warpPerspective(img, M, (int(cols*1.5), int(rows*1.5)))
     cv.imshow('sheared_y-axis_out.jpg', sheared_img)
     cv.waitKey(0)
-    cv.destroyAllWindows()
-
-#Contours
+    cv.destroyAllWindows() 
+    
 def contour():
     import cv2 as cv
     import numpy as np
-    img=cv.imread('Photos/park.jpg')
-    cv.imshow('Park',img)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    cv.imshow('Gray', gray)
+    img=cv.imread('Photos/cat.jpg')
+    gray=cv.cvtcolor(img,cv.COLOR_BGR2GRAY)
     edge=cv.Canny(gray,30,300)
-    contours,hierachy=cv.findContours(edge,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+    contours,hierarchy=cv.findContours(edge,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
     cv.imshow('Canny edges after contouring',edge)
-    print('No. of contours found: ' + str(len(contours)))
-    cv.drawContours(img,contours,-1,(0,255,0),3)
-    cv.imshow('Contours',img)
+    print('Number of contours found=',+str(len(contours)))
+    cv.drawContours(img,contours,-1,(0,255,0),3) #-1 signifies drawing all contours
+    cv.imshow('contours',img)
     cv.waitKey(0)
-    cv.destroyAllWindows()
-
-#Color Spaces
+    cv.destroyAllWindows
+    
 def color_spaces():
     import cv2 as cv
-    img = cv.imread('Photos/park.jpg')
-    B, G, R = cv.split(img)
-
-    cv.imshow("original", img)
+    img=cv.imread('Photos/cat.jpg')
+    B,G,R=cv.split(img)
+    cv.imshow('original',img)
     cv.waitKey(0)
-
-    cv.imshow("blue", B)
+    cv.imshow('blue',B)
     cv.waitKey(0)
-    
-    cv.imshow("Green", G)
+    cv.imshow('green',G)
     cv.waitKey(0)
-    
-    cv.imshow("red", R)
+    cv.imshow('red',R)
     cv.waitKey(0)
-    
-    cv.destroyAllWindows()
 
 #Blurring
-def convolutions():
-    import cv as cv
+def convolution():
+    import cv2
     import numpy as np
-    image = cv.imread('Photos/cats.jpg')
+    image = cv2.imread('Photos/cat.jpg')
     kernel2 = np.ones((5, 5), np.float32)/25
-    img = cv.filter2D(src=image, ddepth=-1, kernel=kernel2)
-    cv.imshow('Original', image)
-    cv.imshow('Kernel Blur', img)
-    cv.waitKey()
-    cv.destroyAllWindows()
+    img = cv2.filter2D(src=image, ddepth=-1, kernel=kernel2)
+    cv2.imshow('Original', image)
+    cv2.imshow('Kernel Blur', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows() 
 
 def averaging():
-    import cv2 as cv
+    import cv2
     import numpy as np
-    image = cv.imread('Photos/cats.jpg')
-    averageBlur = cv.blur(image, (5, 5))
-    cv.imshow('Original', image)
-    cv.imshow('Average blur', averageBlur)
-    cv.waitKey()
-    cv.destroyAllWindows()
-
+    image = cv2.imread('Photos/cat.jpg')
+    averageBlur = cv2.blur(image, (5, 5))
+    cv2.imshow('Original', image)
+    cv2.imshow('Average blur', averageBlur)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    
 def gaussian():
-    import cv2 as cv
+    import cv2
     import numpy as np
-    image = cv.imread('Photos/cats.jpg')
-    gaussian = cv.GaussianBlur(image, (3, 3), 0)
-    cv.imshow('Original', image)
-    cv.imshow('Gaussian blur', gaussian)
-    cv.waitKey()
-    cv.destroyAllWindows()
-
+    image = cv2.imread('Photos/cat.jpg')
+    gaussian = cv2.GaussianBlur(image, (3, 3), 0)
+    cv2.imshow('Original', image)
+    cv2.imshow('Gaussian blur', gaussian)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    
 def median():
-    import cv2 as cv
+    import cv2
     import numpy as np
-    image = cv.imread('Photos/cats.jpg')
-    medianBlur = cv.medianBlur(image, 9)
-    cv.medianBlur(image, 9)
-    cv.imshow('Original', image)
-    cv.imshow('Median blur', 
-    medianBlur)
-    cv.waitKey()
-    cv.destroyAllWindows()
-
+    image = cv2.imread('Photos/cat.jpg')
+    medianBlur=cv2.medianBlur(image, 9)
+    cv2.imshow('Original', image)
+    cv2.imshow('Median blur',medianBlur)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    
 def bilateral():
-    import cv2 as cv
+    import cv2
     import numpy as np
-    image = cv.imread('Photos/cats.jpg')
-    bilateral = cv.bilateralFilter(image, 
-    9, 75, 75)
-    cv.imshow('Original', image)
-    cv.imshow('Bilateral blur', bilateral)
-    cv.waitKey()
-    cv.destroyAllWindows()
+    image = cv2.imread('Photos/cat.jpg')
+    bilateral = cv2.bilateralFilter(image,9, 75, 75)
+    cv2.imshow('Original', image)
+    cv2.imshow('Bilateral blur', bilateral)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
-#Bitwise Operations
-def bitwise_and_():
-    import cv2 as cv
-    import numpy as np
-    blank = np.zeros((400,400), dtype='uint8')
-    rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
-    circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
-    cv.imshow('Rectangle', rectangle)
-    cv.imshow('Circle', circle)
-    bitwise_and = cv.bitwise_and(rectangle, circle)
-    cv.imshow('Bitwise AND', bitwise_and)
-    cv.waitKey(0)
+#Bitwise Operators
+def bitwise_and():
+    import cv2 
+    import numpy as np 
+    img1 = cv2.imread('Photos/input1.png')  
+    img2 = cv2.imread('Photos/input2.png') 
+    dest_and = cv2.bitwise_and(img2, img1, mask = None)
+    cv2.imshow('Bitwise And', dest_and)
+    if cv2.waitKey(0) & 0xff == 27: 
+        cv2.destroyAllWindows()
 
-def bitwise_or_():
-    import cv2 as cv
+def bitwise_or():
+    import cv2
     import numpy as np
-    blank = np.zeros((400,400), dtype='uint8')
-    rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
-    circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
-    cv.imshow('Rectangle', rectangle)
-    cv.imshow('Circle', circle)
-    bitwise_or = cv.bitwise_or(rectangle, circle)
-    cv.imshow('Bitwise OR', bitwise_or)
-    cv.waitKey(0)
+    img1 = cv2.imread('RESOURCES/Photos/input1.png')
+    img2 = cv2.imread('RESOURCES/Photos/input2.png')
+    dest_or = cv2.bitwise_or(img2, img1, mask = None)
+    cv2.imshow('Bitwise OR', dest_or)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
 
-def bitwise_xor_():
-    import cv2 as cv
+def bitwise_xor():
+    import cv2
     import numpy as np
-    blank = np.zeros((400,400), dtype='uint8')
-    rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
-    circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
-    cv.imshow('Rectangle', rectangle)
-    cv.imshow('Circle', circle)
-    bitwise_xor = cv.bitwise_xor(rectangle, circle)
-    cv.imshow('Bitwise XOR', bitwise_xor)
-    cv.waitKey(0)
+    img1 = cv2.imread('Photos/input1.png')
+    img2 = cv2.imread('Photos/input2.png')
+    dest_xor = cv2.bitwise_xor(img1, img2, mask = None)
+    cv2.imshow('Bitwise XOR', dest_xor)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
 
-def bitwise_not_():
-    import cv2 as cv
+def bitwise_not():
+    import cv2
     import numpy as np
-    blank = np.zeros((400,400), dtype='uint8')
-    rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
-    circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
-    cv.imshow('Rectangle', rectangle)
-    cv.imshow('Circle', circle)
-    bitwise_not = cv.bitwise_not(circle)
-    cv.imshow('Circle NOT', bitwise_not)
-    cv.waitKey(0)
+    img1 = cv2.imread('\Photos/input1.png')
+    img2 = cv2.imread('Photos/input2.png')
+    dest_not1 = cv2.bitwise_not(img1, mask = None)
+    dest_not2 = cv2.bitwise_not(img2, mask = None)
+    cv2.imshow('Bitwise NOT on image 1', dest_not1)
+    cv2.imshow('Bitwise NOT on image 2', dest_not2)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
 
-#Masking
-def mask():
+#Masking and Histogram Computation
+def masking():
     import cv2 as cv
     import numpy as np
-    img = cv.imread('Photos/cats.jpg')
+    img = cv.imread('Photos/park.jpg')
     cv.imshow('Original image', img)
     blank = np.zeros(img.shape[:2], dtype='uint8')
     cv.imshow('Blank Image', blank)
@@ -380,30 +385,29 @@ def mask():
     cv.imshow('Masked Image', masked)
     cv.waitKey(0)
 
-def alpha_blending():
+def alpha_blurring():
     import cv2
-    img1 = cv2.imread('Photos/cats2.jpg')
-    img2 = cv2.imread('Photos/cat.jpg')
+    img1 = cv2.imread('Photos/cat.jpg')
+    img2 = cv2.imread('Photos/park.jpg')
+    img2 = cv2.resize(img2, img1.shape[1::-1])
     cv2.imshow("img 1",img1)
     cv2.waitKey(0)
     cv2.imshow("img 2",img2)
     cv2.waitKey(0)
     choice = 1
-    while (choice) :
-        alpha = float(input("Enter alpha value"))
-        dst = cv2.addWeighted(img1, alpha , img2, 
-        1-alpha, 0)
+    while (choice):
+        alpha = float(input("Enter alpha value: "))
+        dst = cv2.addWeighted(img1, alpha , img2, 1-alpha, 0)
         cv2.imwrite('alpha_mask_.png', dst)
         img3 = cv2.imread('alpha_mask_.png')
         cv2.imshow("alpha blending 1",img3)
         cv2.waitKey(0)
-        choice = int(input("Enter 1 to continue and 0 to exit"))
+        choice = int(input("Enter 1 to continue and 0 to exit: "))
 
-#Histogram
 def histogram():
-    import cv2 as cv
+    import cv2
     from matplotlib import pyplot as plt
-    img = cv.imread('Photos/park.jpg',0)
-    histr = cv.calcHist([img],[0],None,[256],[0,256])
+    img = cv2.imread('Photos/park.jpg',0)
+    histr = cv2.calcHist([img],[0],None,[256],[0,256])
     plt.plot(histr)
     plt.show()
